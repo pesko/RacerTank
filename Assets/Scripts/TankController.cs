006a49140d8ee5b7ffc501942f3838289e0b858f
Peter Skogvold <petersk91@gmail.com>
2020-12-12 19:35:37 +0100

    Tank pretty functional
    Some drifting occurs. Not a big issue for now. Added sound, will add VFX soon too

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class TankController : MonoBehaviour
 {
+    #region Serialized Fields
+
     [SerializeField]
     private float _maxSpeed;
     [SerializeField]
@@ -12,13 +14,25 @@ public class TankController : MonoBehaviour
     [SerializeField]
     private Transform _turretPivot;
     [SerializeField]
-    private Quaternion _turretRot;
-
+    private Animator _barrelAnimator;
     [SerializeField]
-    private float _distanceToGround;
+    private AnimationClip _barrelAnimation;
+    [SerializeField]
+    private AudioSource _barrelAudio;
+    [SerializeField]
+    private AudioClip _shotFX;
+
+    #endregion Serialized Fields
+
+    #region Private Fields
 
     private PlayerInput _input;
+    private Vector3 _moveVector;
+    private Quaternion _turretRot;
 
+    #endregion Private Fields
+
+    #region Private Methods
 
     private void KeepTurretRotation()
 	{
@@ -34,74 +48,57 @@ public class TankController : MonoBehaviour
         transform.rotation = Quaternion.Lerp(transform.rotation, rot, step);
 	}
 
-    private bool IsGrounded()
+    private void Drive()
 	{
-        Vector3 down = transform.TransformDirection(Vector3.down);
-
-        if (Physics.Raycast(transform.position, down, 0.3f))
-		{
-            return true;
-		}
-        return false;
-	}
-
-    private void KeepGrounded()
-	{
-        Vector3 down = transform.TransformDirection(Vector3.down);
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position, down, out hit, 5f))
-        {
-            var pos = transform.position;
-            pos.y = hit.point.y;
-            transform.position = pos;
-        }
-    }
-
-    public void OnDrive(InputAction.CallbackContext callback)
-    {
-        var val = callback.ReadValue<Vector2>();
-
-        if (val.x == 0 && val.y == 0)
+        if (_moveVector.x == 0 && _moveVector.y == 0)
         {
             var deceleration = 0.5f * _rb.velocity;
             _rb.velocity = deceleration;
             return;
         }
 
-        var moveVector = new Vector3(val.x, 0, val.y);
-
         var vel = _rb.velocity;
-        vel += moveVector * _acceleration;
+        vel += _moveVector * _acceleration;
         vel.x = Mathf.Clamp(vel.x, -_maxSpeed, _maxSpeed);
         vel.y = Mathf.Clamp(vel.y, -_maxSpeed, _maxSpeed);
 
         _rb.velocity = vel;
-
-        print(vel);
     }
 
-    public void OnTurret(InputValue value)
+    private void Turret()
 	{
-        var val = value.Get<Vector2>();
-        if (val.x == 0 && val.y == 0)
-        { 
-            return; 
-        }
-
-        var joystickRot = new Vector3(val.x, 0, val.y);
-
-        var lookrot = Quaternion.LookRotation(joystickRot);
-        if (val.x != 0 && val.y != 0)
-        {
-            _turretPivot.rotation = Quaternion.Lerp(_turretPivot.transform.rotation, lookrot, 0.1f);
-            _turretRot = _turretPivot.rotation;
-        }
+        _turretPivot.rotation = Quaternion.Lerp(_turretPivot.transform.rotation, _turretRot, 0.25f);
     }
 
-    public void OnShoot(InputValue value)
+    private void ApplyGravity()
 	{
+        var vel = _rb.velocity;
 
+        vel.y -= Physics.gravity.y;
 	}
+
+    private void OnDrive(InputAction.CallbackContext callback)
+    {
+        var v = callback.ReadValue<Vector2>();
+        _moveVector = new Vector3(v.x, 0, v.y);
+    }
+
+    private void OnTurret(InputAction.CallbackContext callback)
+    {
+        var val = callback.ReadValue<Vector2>();
+        var v3 = new Vector3(val.x, 0, val.y);
+        _turretRot = Quaternion.LookRotation(v3);
+    }
+
+    private void OnShoot(InputAction.CallbackContext callback)
+    {
+        _barrelAnimator.Play(_barrelAnimation.name);
+        _barrelAudio.PlayOneShot(_shotFX);
+    }
+
+    #endregion Private Methods
+
+    #region Unity Callbacks
 
     void Start()
     {
@@ -110,18 +107,29 @@ public class TankController : MonoBehaviour
         _input = InputMaster.Instance.PlayerInput;
 
 		_input.Tank.Drive.performed += OnDrive;
+        _input.Tank.Turret.performed += OnTurret;
+        _input.Tank.Shoot.performed += OnShoot;
     }
 
-	// Update is called once per frame
-	void Update()
+    // Update is called once per frame
+    void Update()
     {
         FaceVelocity();
-        KeepTurretRotation();
-        //KeepGrounded();
     }
+
+	protected void FixedUpdate()
+	{
+        Drive();
+        Turret();
+        ApplyGravity();
+	}
 
 	private void OnDestroy()
 	{
 		_input.Tank.Drive.performed -= OnDrive;
+        _input.Tank.Turret.performed -= OnTurret;
+        _input.Tank.Shoot.performed -= OnShoot;
     }
+
+	#endregion Unity Callbacks
 }
